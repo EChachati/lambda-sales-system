e38940aae3cc7b82509e4a90f02c278dc430244e
Edkar Chachati <chachati28@gmail.com>
2021-07-19 23:00:25 -0400

    created UserModel

--- core/models.py
+++ core/models.py
@@ -1,1 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+from django.conf import settings
+
+
+class UserManager(BaseUserManager):
+    """
+    Manager For Users
+    """
+
+    def create_user(self, email, password=None, **extra_fields):
+
+        if not email:
+            raise ValueError("Please Provide an Email")
+
+        user = self.model(
+            email=email.lower(),
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Personalized model for Users support login with Email
+    """
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
