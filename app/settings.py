ca87d0cc64e7ae13a345ead2b8109a152f11252d
Edkar Chachati <chachati28@gmail.com>
2021-07-21 13:25:02 -0400

    apparently done user API, created product API

--- app/settings.py
+++ app/settings.py
@@ -109,7 +109,11 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
