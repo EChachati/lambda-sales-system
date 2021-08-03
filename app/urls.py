30c9c26a9d13ff94ebb3d0371780df2c9390263b
Edkar Chachati <chachati28@gmail.com>
2021-08-03 19:32:34 -0400

    Created Clients App

--- app/urls.py
+++ app/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
     path('api/product/', include('product.urls')),
-    path('api/salesman/', include('salesman.urls'))
+    path('api/salesman/', include('salesman.urls')),
+    path('api/client/', include('client.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
