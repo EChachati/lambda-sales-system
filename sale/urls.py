b79eb15c017dc90ef2c36fe55068365bf61a9c37
Edkar Chachati <chachati28@gmail.com>
2021-08-07 18:56:00 -0400

    Can create Sales and Product Sales

--- sale/urls.py
+++ sale/urls.py
@@ -4,8 +4,8 @@ from rest_framework.routers import DefaultRouter
 from sale import views
 
 router = DefaultRouter()
+router.register('product-sale', views.PSViewSet)
 router.register('', views.SaleViewSet)
-
 app_name = 'sale'
 
 urlpatterns = [
