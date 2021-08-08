9a28d9128a6b2ef5e9e994543ae6d3adddf71f91
Edkar Chachati <chachati28@gmail.com>
2021-08-08 16:18:03 -0400

    Client and Salesman updates when sale created

--- sale/views.py
+++ sale/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 
-from core.models import Sale, ProductSale
+from core.models import Sale, ProductSale, Salesman, Client
 from sale import serializers
+
+import decimal
 
 
 class SaleViewSet(viewsets.ModelViewSet):
@@ -10,6 +12,25 @@ class SaleViewSet(viewsets.ModelViewSet):
     """
     queryset = Sale.objects.all()
     serializer_class = serializers.SaleSerializer
+
+    def create(self, request):
+        """
+        Update values in client and salesman models
+        """
+        instance = super().create(request)
+
+        income = request.data['income']
+
+        salesman = Salesman.objects.get(pk=request.data['salesman'])
+        salesman.purchases += 1
+        salesman.money_generated += decimal.Decimal(income)
+        salesman.save()
+
+        client = Client.objects.get(pk=request.data['client'])
+        client.purchases += 1
+        client.money_spent += decimal.Decimal(income)
+        client.save()
+        return instance
 
 
 class ProductSaleViewSet(viewsets.ModelViewSet):
