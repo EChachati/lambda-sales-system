3b159e7e288e5261e61bfacd69f6a054206c8a66
Edkar Chachati <chachati28@gmail.com>
2021-08-28 21:25:44 -0400

    Finished tests

--- salesman/urls.py
+++ salesman/urls.py
@@ -4,7 +4,8 @@ from rest_framework.routers import DefaultRouter
 from salesman import views
 
 router = DefaultRouter()
-router.register('indicator', views.SalesmanIndicatorsViewSet)
+router.register('indicator', views.SalesmanIndicatorsViewSet,
+                basename='indicator')
 router.register('', views.SalesmanViewSet)
 
 app_name = 'salesman'
